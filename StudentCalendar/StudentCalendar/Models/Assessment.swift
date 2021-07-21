c0332003a57f249bb2c6560a7aed5d654251f27e
ahavamorse <ahavamorse@icloud.com>
2021-07-21 09:15:05 -0700

    Added assessment model and variables

---
+++ StudentCalendar/StudentCalendar/Models/Assessment.swift
@@ -0,0 +1,19 @@
+//
+//  Assessment.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/21/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import Foundation
+
+struct Assesment {
+    var title: String
+    var date: Date
+    var subject: Subject
+    var pointValue: Int
+    var score: Int
+    var notes: String
+    var type: AssesmentType
+}
