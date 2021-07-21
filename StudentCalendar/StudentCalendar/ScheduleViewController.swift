637c555399cd43127cbde7c3a2c8f89031a6db86
ahavamorse <ahavamorse@icloud.com>
2021-07-21 09:43:19 -0700

    Created the schedule view controller

---
+++ StudentCalendar/StudentCalendar/ScheduleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScheduleViewController.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/21/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
