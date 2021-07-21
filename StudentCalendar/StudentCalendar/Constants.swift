2a668c9fa092215a4d1588f67d85351e855a81c3
ahavamorse <ahavamorse@icloud.com>
2021-07-21 08:51:03 -0700

    Added subject and display color constants

---
+++ StudentCalendar/StudentCalendar/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/21/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+enum SubjectColor: String {
+    case gray = "Gray"
+    case red = "Red"
+    case orange = "Orange"
+    case yellow = "Yellow"
+    case green = "Green"
+    case blue = "Blue"
+    case purple = "Purple"
+    case pink = "Pink"
+}
+
+enum DisplayColor {
+    static let gray = UIColor.systemGray
+    static let red = UIColor.systemRed
+    static let orange = UIColor.systemOrange
+    static let yellow = UIColor.systemYellow
+    static let green = UIColor.systemGreen
+    static let blue = UIColor.systemBlue
+    static let purple = UIColor.systemPurple
+    static let pink = UIColor.systemPink
+}
