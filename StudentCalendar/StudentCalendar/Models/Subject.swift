d84cd237d2600d8731ad550c71064d8b1bd15768
ahavamorse <ahavamorse@icloud.com>
2021-07-21 08:38:21 -0700

    Added subject model variables

---
+++ StudentCalendar/StudentCalendar/Models/Subject.swift
@@ -0,0 +1,17 @@
+//
+//  Subject.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/21/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import Foundation
+
+struct Subject {
+    var name: String
+    var instructor: String
+    var startDate: Date
+    var endDate: Date
+    var color: SubjectColor
+}
