246c4c0fd53cbcb3d5185d021766f1b327e12773
ahavamorse <ahavamorse@icloud.com>
2021-07-22 09:48:42 -0700

    Implemented class controller

--- StudentCalendar/StudentCalendar/Controllers/ClassController.swift
+++ StudentCalendar/StudentCalendar/Controllers/ClassController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class ClassController {
+    
+    private var classes: [Date: [Class]] = [:]
+    
+    init(classes: [Class] = []) {
+        add(classes)
+    }
+    
+    func add(_ newClasses: [Class]) {
+        // put assignments in dictrionary by date
+        for newClass in newClasses {
+            if classes[newClass.date] == nil {
+                classes[newClass.date] = [newClass]
+            } else {
+                classes[newClass.date]?.append(newClass)
+            }
+        }
+    }
+    
+    func remove(_ oldClass: Class) {
+        // remove assignment from list
+        let date = oldClass.date
+        if classes[date]?.count == 1 {
+            classes.removeValue(forKey: date)
+        } else {
+            if let index = classes[date]?.firstIndex(of: oldClass) {
+                classes[date]?.remove(at: index)
+            }
+        }
+    }
+    
+    func getClasses() -> [Class] {
+        // return assignments in sorted order
+        let sortedDates = classes.keys.sorted()
+        var sortedClasses: [Class] = []
+        for key in sortedDates {
+            sortedClasses += classes[key] ?? []
+        }
+        return sortedClasses
+    }
+}
