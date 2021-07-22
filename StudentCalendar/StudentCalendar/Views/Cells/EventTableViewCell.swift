55dd37864ffdec3e04d2a9a3aee75dcaf7db0716
ahavamorse <ahavamorse@icloud.com>
2021-07-22 11:50:55 -0700

    Added a time label to the event table view cell

--- StudentCalendar/StudentCalendar/Views/Cells/EventTableViewCell.swift
+++ StudentCalendar/StudentCalendar/Views/Cells/EventTableViewCell.swift
@@ -12,7 +12,8 @@ class EventTableViewCell: UITableViewCell {
     
     static let reuseID = "EventTableViewCell"
     let subjectColorImageView = UIImageView()
-    let titleLabel = TitleLabel(textAlignment: .left)
+    let titleLabel = TitleLabel(font: .preferredFont(forTextStyle: .title1))
+    let timeLabel = SecondaryTitleLabel(textAlignment: .right, font: .preferredFont(forTextStyle: .title2))
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -26,11 +27,11 @@ class EventTableViewCell: UITableViewCell {
     func set(event: Event) {
         subjectColorImageView.image = UIImage(systemName: "circle.fill")!.withTintColor(event.subject.color)
         titleLabel.text = event.title
+        timeLabel.text = event.timeString
     }
     
     private func configure() {
-        addSubview(subjectColorImageView)
-        addSubview(titleLabel)
+        addSubviews(subjectColorImageView, titleLabel, timeLabel)
         subjectColorImageView.translatesAutoresizingMaskIntoConstraints = false
         
         accessoryType = .disclosureIndicator
@@ -42,9 +43,14 @@ class EventTableViewCell: UITableViewCell {
             subjectColorImageView.heightAnchor.constraint(equalToConstant: 20), // customize
             subjectColorImageView.widthAnchor.constraint(equalToConstant: 20), // customize
             
+            timeLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            timeLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 70),
+            timeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30 - padding),
+            timeLabel.heightAnchor.constraint(equalToConstant: 40), // customize
+            
             titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: subjectColorImageView.trailingAnchor, constant: padding),
-            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            titleLabel.trailingAnchor.constraint(equalTo: timeLabel.leadingAnchor, constant: -padding),
             titleLabel.heightAnchor.constraint(equalToConstant: 40) // customize
         ])
     }
