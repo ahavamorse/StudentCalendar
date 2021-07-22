8ffd20c768fbd9f19d6394221223fc625d64d3a4
ahavamorse <ahavamorse@icloud.com>
2021-07-22 09:49:56 -0700

    Conformed class model to equatable

--- StudentCalendar/StudentCalendar/Models/Class.swift
+++ StudentCalendar/StudentCalendar/Models/Class.swift
@@ -8,9 +8,20 @@
 
 import Foundation
 
-struct Class: Event {
+struct Class: Event, Equatable {
     var title: String
     var date: Date  // start time
     var endTime: Date
     var subject: Subject
+    
+    static func == (lhs: Class, rhs: Class) -> Bool {
+        if lhs.title == rhs.title,
+            lhs.date == rhs.date,
+            lhs.endTime == rhs.endTime,
+            lhs.subject.title == rhs.subject.title {
+            return true
+        } else {
+            return false
+        }
+    }
 }
