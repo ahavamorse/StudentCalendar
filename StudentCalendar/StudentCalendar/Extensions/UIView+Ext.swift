8c4a0bb64fb1b7e1633fee46423ee3e73c778991
ahavamorse <ahavamorse@icloud.com>
2021-07-22 11:47:38 -0700

    Added a UI view extension to allow adding more than one subview at a time

---
+++ StudentCalendar/StudentCalendar/Extensions/UIView+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Ext.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/22/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        for view in views { addSubview(view) }
+    }
+}
