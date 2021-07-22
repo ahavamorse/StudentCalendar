e556e2f8fd033acab6ad12a82599bd1df881fe82
ahavamorse <ahavamorse@icloud.com>
2021-07-22 09:18:40 -0700

    Created assignment controller class, variables and function stubs

--- StudentCalendar/StudentCalendar/Controllers/AssignmentController.swift
+++ StudentCalendar/StudentCalendar/Controllers/AssignmentController.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class AssignmentController {
+    
+    private var allAssignments: [Assignment]
+    private var assignments: [Date: [Assignment]] = [:]
+    
+    init(assignments: [Assignment] = []) {
+        self.allAssignments = assignments
+        self.sortAssignments()
+    }
+    
+    private func sortAssignments() {
+        // todo put assignments in order by date starting with soonest
+    }
+    
+    func add(assignment: Assignment) {
+        // put new assignment in the correct spot
+    }
+    
+    func remove(assignment: Assignment) {
+        // remove assignment from list
+    }
+    
+    func getAssignments() {
+        // return assignments in sorted order
+    }
+    
+}
