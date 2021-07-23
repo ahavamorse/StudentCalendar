07bd2c5788ceac400d0ff6a17f799e6a11cdb571
ahavamorse <ahavamorse@icloud.com>
2021-07-23 09:37:53 -0700

    Implemented task table view cell configure function

--- StudentCalendar/StudentCalendar/Views/Cells/TaskTableViewCell.swift
+++ StudentCalendar/StudentCalendar/Views/Cells/TaskTableViewCell.swift
@@ -14,6 +14,7 @@ class TaskTableViewCell: UITableViewCell {
     let subjectColorImageView = UIImageView()
     let titleLabel = TitleLabel(font: .preferredFont(forTextStyle: .title1))
     let dueDateLabel = SecondaryTitleLabel(font: .preferredFont(forTextStyle: .title2))
+    let checkBoxButton = UIButton()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -24,5 +25,33 @@ class TaskTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    private func configure() {
+        addSubviews(subjectColorImageView, titleLabel, dueDateLabel, checkBoxButton)
+        subjectColorImageView.translatesAutoresizingMaskIntoConstraints = false
+        checkBoxButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            subjectColorImageView.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            subjectColorImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            subjectColorImageView.heightAnchor.constraint(equalToConstant: 20),
+            subjectColorImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            checkBoxButton.topAnchor.constraint(equalTo: self.topAnchor, constant: padding),
+            checkBoxButton.heightAnchor.constraint(equalToConstant: 40),
+            checkBoxButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            checkBoxButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: padding),
+            titleLabel.leadingAnchor.constraint(equalTo: subjectColorImageView.trailingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: checkBoxButton.leadingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 25),
+            
+            dueDateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: padding),
+            dueDateLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            dueDateLabel.trailingAnchor.constraint(equalTo: checkBoxButton.leadingAnchor, constant: -padding),
+            dueDateLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -padding)
+        ])
+    }
 }
