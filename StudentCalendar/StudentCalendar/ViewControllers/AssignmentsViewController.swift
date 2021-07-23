4c62ec4c3e52387d02402ce42ee80cc3a4342ceb
ahavamorse <ahavamorse@icloud.com>
2021-07-23 08:51:00 -0700

    Created assignments view controller

---
+++ StudentCalendar/StudentCalendar/ViewControllers/AssignmentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AssignmentsViewController.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/23/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
