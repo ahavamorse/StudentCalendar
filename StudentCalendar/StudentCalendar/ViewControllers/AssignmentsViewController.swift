7e90f453eef426ff60fd91f8503d80864e200e49
ahavamorse <ahavamorse@icloud.com>
2021-07-23 09:03:12 -0700

    Added assignments view controller extension and table view delegate and data source function stubs

--- StudentCalendar/StudentCalendar/ViewControllers/AssignmentsViewController.swift
+++ StudentCalendar/StudentCalendar/ViewControllers/AssignmentsViewController.swift
@@ -59,3 +59,13 @@ class AssignmentsViewController: UIViewController {
         }
     }
 }
+
+extension AssignmentsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
