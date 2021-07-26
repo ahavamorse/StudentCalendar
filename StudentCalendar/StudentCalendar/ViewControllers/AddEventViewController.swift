29fd9bd42754e598854950bf9845b036f73ca278
ahavamorse <ahavamorse@icloud.com>
2021-07-26 14:46:32 -0700

    Added constraints to add event view controller

--- StudentCalendar/StudentCalendar/ViewControllers/AddEventViewController.swift
+++ StudentCalendar/StudentCalendar/ViewControllers/AddEventViewController.swift
@@ -44,7 +44,7 @@ class AddEventViewController: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         
-        view.addSubviews(/*eventTypeSegmentedControl), */titleTextField)/*, subjectLabel, subjectPickerView, dateLabel, datePickerView)*/
+        view.addSubviews(eventTypeSegmentedControl, titleTextField, subjectLabel, subjectPickerView, dateLabel, datePickerView)
         
         eventTypeSegmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
         
@@ -54,6 +54,37 @@ class AddEventViewController: UIViewController {
         subjectPickerView.dataSource = self
         dateLabel.text = "Date:" // can change
         datePickerView.datePickerMode = .dateAndTime
+        
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            eventTypeSegmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            eventTypeSegmentedControl.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            eventTypeSegmentedControl.widthAnchor.constraint(equalToConstant: 50),
+            
+            titleTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            titleTextField.heightAnchor.constraint(equalToConstant: 22),
+            
+            subjectLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: padding),
+            subjectLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            subjectLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            subjectLabel.heightAnchor.constraint(equalToConstant: 25),
+            
+            subjectPickerView.topAnchor.constraint(equalTo: subjectLabel.bottomAnchor, constant: padding),
+            subjectPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            subjectPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            
+            dateLabel.topAnchor.constraint(equalTo: subjectPickerView.bottomAnchor, constant: padding),
+            dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            dateLabel.heightAnchor.constraint(equalToConstant: 25),
+            
+            datePickerView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: padding),
+            datePickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            datePickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+        ])
     }
     
     @objc func segmentedControlValueChanged() {
