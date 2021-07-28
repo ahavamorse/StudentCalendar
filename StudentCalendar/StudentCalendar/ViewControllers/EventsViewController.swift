f5e497f9dc2c45bc9a95872a95449176d9ff294f
ahavamorse <ahavamorse@icloud.com>
2021-07-28 15:21:59 -0700

    Created event view controller for assignments, classes, and assessments view controllers to inherit from

---
+++ StudentCalendar/StudentCalendar/ViewControllers/EventsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventsViewController.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 7/28/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
