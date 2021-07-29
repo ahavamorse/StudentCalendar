d66157799d9f3bab066788a70ac011a16ae07784
ahavamorse <ahavamorse@icloud.com>
2021-07-29 09:13:03 -0700

    Implemented class table view cell initializers

--- StudentCalendar/StudentCalendar/Views/Cells/ClassTableViewCell.swift
+++ StudentCalendar/StudentCalendar/Views/Cells/ClassTableViewCell.swift
@@ -18,4 +18,13 @@ class ClassTableViewCell: UITableViewCell {
     
     var cellClass: Class?
     weak var delegate: ClassesViewController?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
