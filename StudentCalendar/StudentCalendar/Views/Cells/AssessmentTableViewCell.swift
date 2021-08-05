2b02eb44b6ca91299965b3f774ce378335b5c8b0
ahavamorse <ahavamorse@icloud.com>
2021-08-05 12:17:02 -0700

    Created AssessmentTableViewCell

---
+++ StudentCalendar/StudentCalendar/Views/Cells/AssessmentTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AssessmentTableViewCell.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 8/5/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class AssessmentTableViewCell: UITableViewCell {
+
+    static let reuseID = "AssessmentTableViewCell"
+    
+    func set(assessment: Assessment) {
+        
+    }
+
+}
