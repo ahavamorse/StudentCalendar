22807a9cd4314c87f4028da0b6d02488361bc11f
ahavamorse <ahavamorse@icloud.com>
2021-08-05 15:06:26 -0700

    Created SubjectsViewController and variables

---
+++ StudentCalendar/StudentCalendar/ViewControllers/SubjectsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SubjectsViewController.swift
+//  StudentCalendar
+//
+//  Created by HapiDani on 8/5/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class SubjectsViewController: UIViewController {
+    
+    var tableView = UITableView()
+    var subjectController: SubjectController!
+    var subjects: [Subject] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
